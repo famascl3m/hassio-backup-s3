e49deca1208dea4d57df94f3f26a0aeda0e29030
famascl3m <40993037+famascl3m@users.noreply.github.com>
2023-11-14 17:23:58 +0100

    Update run.sh
    added endpoint url

--- backup-s3/run.sh
+++ backup-s3/run.sh
@@ -2,6 +2,7 @@
 
 KEY=`jq -r .awskey /data/options.json`
 SECRET=`jq -r .awssecret /data/options.json`
+ENDPOINT=`jq -r .endpoint /data/options.json`
 BUCKET=`jq -r .bucketname /data/options.json`
 
 now="$(date +'%d/%m/%Y - %H:%M:%S')"
@@ -10,6 +11,7 @@ echo $now
 
 aws configure set aws_access_key_id $KEY
 aws configure set aws_secret_access_key $SECRET
+aws configure set endpoint_url $ENDPOINT
 
 aws s3 sync /backup/ s3://$BUCKET/  --delete
 
